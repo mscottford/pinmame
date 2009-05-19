fbb8a251740a86490c00ffb24e3eaa08f62efb08
M. Scott Ford <vaderpi@nypoint.newyawk.us>
2009-05-18 21:27:33 -0400

    Removed installation code for Visual PinMame.

--- src/instvpm/Globals.cpp
+++
@@ -1,4 +0,0 @@
-#include "Windows.h"
-
-HINSTANCE	g_hInstance = 0;
-char		g_szCaption[256];
--- src/instvpm/TestVPinMAME.h
+++
@@ -1,7 +0,0 @@
-#ifndef TESTVPINMAME_H
-#define TESTVPINMAME_H
-
-const char* GetInstalledVersion(char* szVersion, int iSize);
-void RunTest(HWND hWnd);
-
-#endif // TESTVPINMAME_H
